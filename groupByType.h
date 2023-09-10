22ac55f95b4f987e129aaf8d1b65b8c58e356a6d
aleksey_geshko <89023870+AlekseyGeshko@users.noreply.github.com>
2023-09-10 12:57:39 +0300

    Update groupByType.h

--- groupByType.h
+++ groupByType.h
@@ -13,19 +13,22 @@ namespace group {
         using type_groups = std::unordered_map<std::string_view,
                 std::pair<int, std::vector<const object *>>>;
 
-        void createGroups(const std::vector<object> &data);
+        void createGroups(const std::vector<object>& data);
 
-        void addElements(std::vector<const object *> &elements, std::vector<const object *> objects);
+        void addElements(std::vector<const object *>& objects);
 
+        void sortGroup(std::vector<const object *>& elements);
     public:
         groupByType(int _need_objects);
 
-        void group(const std::vector<object> &data) override;
+        void group(const std::vector<object>& data) override;
 
-        void printInFile(std::ofstream &out) override;
+        void printInFile(std::ofstream& out) const override;
 
     private:
         type_groups my_groups;
+        std::vector<const object *> difference_elements;
         int need_objects;
     };
 }
+
