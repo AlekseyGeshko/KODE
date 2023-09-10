edaaef1ae91cd0e774ca519a58bdba2a3b0bb894
aleksey_geshko <89023870+AlekseyGeshko@users.noreply.github.com>
2023-09-10 12:55:56 +0300

    Update groupByName.cpp

--- groupByName.cpp
+++ groupByName.cpp
@@ -2,6 +2,14 @@
 
 using namespace group;
 
+void groupByName::sortGroups(){
+    for (auto &[_, objects]: groups) {
+        std::sort(objects.begin(), objects.end(),[](const object* lhs, const object* rhs){
+            return lhs->name < rhs->name;
+        });
+    }
+}
+
 void groupByName::group(const std::vector<object>& data) {
     const std::string ru_alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
                                 "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
@@ -12,12 +20,13 @@ void groupByName::group(const std::vector<object>& data) {
             groups["#"].push_back(&_object_);
         }
     }
+    sortGroups();
 }
 
-void groupByName::printInFile(std::ofstream& out) {
+void groupByName::printInFile(std::ofstream& out) const {
     out << "--------Группировка по имени--------\n";
     for (auto &[first_symb, objects]: groups) {
         out << first_symb << '\n';
-        printGroup(sortGroup(objects), out);
+        printGroup(objects, out);
     }
 }
